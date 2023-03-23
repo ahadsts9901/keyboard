668dfb1c5047b1740040c69e1b4d21d64ff4f74d
ahadsts9901 <ahadsts990@gmail.com>
2023-03-23 12:38:48 -0700

    done but not responsive

---
+++ main.js
@@ -0,0 +1,15 @@
+function getnumber (num) {
+    
+    let result = document.getElementById("text");
+
+    result.value += num
+
+}
+
+function Back() {
+
+    var str = document.getElementById('text').value
+    str = str.substr(0,str.length-1)
+    document.getElementById('text').value = str
+
+}
